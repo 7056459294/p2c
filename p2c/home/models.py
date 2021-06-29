7dc650fb466a34a01fe996954da1cdfcbfe76694
Nikhil <Nikhl@gmal.com>
2021-06-29 11:22:20 +0530

    events section

--- p2c/home/models.py
+++ p2c/home/models.py
@@ -1,3 +1,15 @@
+from django.db import models #p2c-21-085
+
+
+class Events(models.Model):   #p2c-21-085
+    events_name = models.CharField(max_length=500) #p2c-21-085
+    event_organizer = models.CharField(max_length=100) #p2c-21-085
+    organizer_contact = models.IntegerField(blank=True) #p2c-21-085
+
+    def __str__(self): #p2c-21-085
+        return self.events_name #p2c-21-085
+
+
 from django.db import models
 
 # Create your models here.
--- p2c/home/views.py
+++ p2c/home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render #p2c-21-085
+from .models import *#p2c-21-085
+def index(request):#p2c-21-085
+    events = Events.objects.all() #p2c-21-085
+    return render(request,'event.html',{'events':events}) #p2c-21-085
 
 # Create your views here.
-
-
